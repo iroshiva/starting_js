9ef2db519198624920a7b63aaabfd005d52a1562
iroshiva <jean.weil@protonmail.com>
2019-08-22 18:38:25 +0200

    script_7 done

--- script_6.js
+++ script_6.js
@@ -1,10 +1,39 @@
 
-// let arn = "CCGUCGUUGCGCUACAGC";
+let arn = "CCGUCGUUGCGCUACAGC";
+// == code ARN à déchiffrer
 
-// // var arn_codon = arn.match(/.{3}|.{1,2}/g);
+function acid_amin(sent){
+// fonction qui prend en entrée le code sous forme de string
+
+	let array = sent.match(/.{1,3}/g);
+// coupe le code toute les trois lettres et sort un array contenant les codons
+
+	for(i = 0; i < array.length; i++){
+// pour chaque index, regarde si le codon correspond à un acide aminé
+// si oui, remplace le codon par son acide aminé correspondant
+
+		if(["UCU" , "UCC" , "UCA" , "UCG" , "AGU" , "AGC"].includes(array[i])){
+			array.splice(i,1, 'Sérine');
+		}else if(["CCU", "CCC" , "CCA" , "CCG"].includes(array[i])){
+			array.splice(i,1, 'Proline');
+		}else if(["UUA" , "UUG"].includes(array[i])){
+			array.splice(i,1, 'Leucine');
+		}else if(["UUU" , "UUC"].includes(array[i])){
+			array.splice(i,1, 'Phénylalanine');
+		}else if(["CGU" , "CGC" , "CGA" , "CGG" , "AGA" , "AGG"].includes(array[i])){
+			array.splice(i,1, 'Arginine');
+		}else if(["UAC" , "UAC"].includes(array[i])){
+			array.splice(i,1, 'Tyrosine');
+		}
+	}
+	console.log(array.join('-'));
+}
+
+acid_amin(arn);
 
 
-// // console.log(arn_codon);
+
+// let arn = "CCUCGCCGGUACUUCUCG";
 
 // function acid_amin(sent){
 
@@ -30,32 +59,3 @@
 // }
 
 // acid_amin(arn);
-
-
-
-let arn = "CCUCGCCGGUACUUCUCG";
-
-function acid_amin(sent){
-
-	let array = sent.match(/.{1,3}/g);
-	// console.log(array)
-
-	for(i = 0; i < array.length; i++){
-		if(["UCU" , "UCC" , "UCA" , "UCG" , "AGU" , "AGC"].includes(array[i])){
-			array.splice(i,1, 'Sérine');
-		}else if(["CCU", "CCC" , "CCA" , "CCG"].includes(array[i])){
-			array.splice(i,1, 'Proline');
-		}else if(["UUA" , "UUG"].includes(array[i])){
-			array.splice(i,1, 'Leucine');
-		}else if(["UUU" , "UUC"].includes(array[i])){
-			array.splice(i,1, 'Phénylalanine');
-		}else if(["CGU" , "CGC" , "CGA" , "CGG" , "AGA" , "AGG"].includes(array[i])){
-			array.splice(i,1, 'Arginine');
-		}else if(["UAC" , "UAC"].includes(array[i])){
-			array.splice(i,1, 'Tyrosine');
-		}
-	}
-	console.log(array.join('-'));
-}
-
-acid_amin(arn);
