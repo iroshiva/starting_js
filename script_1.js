59385dc3a5fbce7bc36d0a0fcfa54f3c28e38e64
iroshiva <jean.weil@protonmail.com>
2019-08-22 09:21:53 +0200

    exo1 done

---
+++ script_1.js
@@ -0,0 +1,14 @@
+
+// Bonjour
+
+console.log("Bonjour Monde");
+
+// Salut
+
+let firstName = prompt("Salut toi, quel est ton joli prénom?");
+
+let answer = `Salut ${firstName}, on va boire un coup?`;
+// ATTENTION `` NE SONT PAS DES SIMPLE QUOTE !!!!!!!!
+
+console.log(answer);
+
---
+++ script_2.js
@@ -0,0 +1,4 @@
+
+
+let number = prompt("Balance un chiffre ou un nombre et je te donnerai son factoriel!");
+
